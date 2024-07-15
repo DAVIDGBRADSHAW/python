7bbff276b122088cac75120df9374c9971b48079
David BRADSDHAW <133218136+DAVIDGBRADSHAW@users.noreply.github.com>
2024-07-15 20:14:15 +0100

    Add files via upload

---
+++ templates/script.js
@@ -0,0 +1,12 @@
+alert('PLEASE KEY IN YOUR......')
+
+function do(){
+    prompt('ddddddddc');
+}
+do();
+
+let person = prompt("Please enter you account number");
+
+if (person != null){
+    document.getElementById("dave bra").innerHTML =
+    "hello davey " + person +  "how are you todau";}
