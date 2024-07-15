ccb12795bffae62d2e877f68363626ca40a2ba1b
David BRADSDHAW <133218136+DAVIDGBRADSHAW@users.noreply.github.com>
2024-07-15 20:16:35 +0100

    Add files via upload

--- app2.py
+++ app2.py
@@ -1,31 +1,39 @@
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+@app.route("/")
+@app.route("/info")
+def home():
+    return render_template("info.html")
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
 
 
-app = Flask(__name__)
 @app.route("/")
 @app.route("/photos")
 def photos():
-    return render_template("home.html")
+    return render_template("photos.html")
 
-app = Flask(__name__)
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template("home.html")
-
-
-app = Flask(__name__)
 @app.route("/")
 @app.route("/services")
-def servies():
+def services():
     return render_template("home.html")
+    return "<h1>  home page displaying ...</h1>"
+
+@app.route("/")
+@app.route("/contact")
+def contact():
     return "<h1> contact page displaying ...</h1>"
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 # python/templates/.............
 # python/templates/home.html   
 #jinger elements render to html
